6403c7e25c8c1ac36ac2772a2c7b2a9096da4f13
geru-scotland <aingeru.scotland@gmail.com>
2023-08-24 14:17:15 +0200

    Add sad path test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,3 +135,9 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+        
